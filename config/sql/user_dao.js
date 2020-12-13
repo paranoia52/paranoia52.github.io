f420c62226236436b5d430663239ceb5658140e1
paranoia52 <57613015+paranoia52@users.noreply.github.com>
2020-12-13 20:07:10 +0800

    1

--- config/sql/user_dao.js
+++ config/sql/user_dao.js
@@ -1,23 +1,29 @@
 let { pool } = require("../mysqlConf.js")
 
 module.exports = {
+  login: function (params, callback) { // 登录操作
+    pool.query("SELECT id,username FROM users where username = ? and password = ?;", [params.username, params.password], function (error, result) {
+      if (error) throw error;
+      callback(result);
+    });
+  },
   add: function (user, callback) { // users表中增加user操作
     let sqlparam = [
       user.username ? user.username : null,
       user.password ? user.password : null
     ]
-    pool.query("SELECT * FROM users WHERE username = ?;", sqlparam[0],function (error, result){
+    pool.query("SELECT * FROM users WHERE username = ?;", sqlparam[0], function (error, result) {
       if (error) throw error;
-      if(result.length){
+      if (result.length) {
         callback('账号已存在')
-      } else{
+      } else {
         pool.query("INSERT INTO users (username , password) VALUES (?, ?);", sqlparam, function (error, result) {
           if (error) throw error;
           callback(result);
         });
       }
     })
-    
+
   },
   deleted: function (params, callback) { // users表中删除指定user操作
     let { id } = params
@@ -43,12 +49,6 @@ module.exports = {
   },
   update: function (params, callback) { // users表中更新user操作
     pool.query("UPDATE users set username= ? where id = ?;", [params.username, params.id], function (error, result) {
-      if (error) throw error;
-      callback(result);
-    });
-  },
-  login: function (params, callback) { // 登录操作
-    pool.query("SELECT id,username FROM users where username = ? and password = ?;", [params.username, params.password], function (error, result) {
       if (error) throw error;
       callback(result);
     });
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '后台主页' });
-});
-router.get('/home', function (req, res, next) {
-  res.render('home', { title: '' });
+  res.render('index', { title: '后端主页' });
 });
 
 
