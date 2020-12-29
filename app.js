1347e10235787d0517e9c5c9b74254953c4ad16f
戚健运 <1142920583@qq.com>
2020-12-29 20:27:34 +0800

    1

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var ejs = require('ejs') // 设置模板引擎
-require('./config/mysqlConf.js');
+require('./config/mysqlConf.js'); // 操作数据库文件
+const multer = require('multer'); // 上传文件依赖
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/modules/users');
@@ -19,7 +20,7 @@ app.set('view engine', 'html')
 
 app.all('*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "content-type");
+  res.header("Access-Control-Allow-Headers", "content-type,token");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   // res.header("Content-Type", "application/json;charset=utf-8");
   res.header("X-Powered-By", ' 3.2.1')
@@ -36,6 +37,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/upload')
+  },
+  filename: function (req, file, cb) {
+    var fileFormat = (file.originalname).split(".");
+    cb(null, fileFormat[0] + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+  }
+})
+var upload = multer({ storage: storage })
+app.post('/upload', upload.any(), function (req, res, next) {
+  console.log(req.files[0]);  // 上传的文件信息
+  res.send('{ msg : "upload seccessed",url: "http://127.0.0.1:3000/upload/' + req.files[0].filename + '"}')
+})
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
