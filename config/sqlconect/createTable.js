9929cc6fcfd45d747cd5c6e77293f4d4a0594c13
戚健运 <1142920583@qq.com>
2021-02-21 18:26:33 +0800

    1

--- config/sqlconect/createTable.js
+++ config/sqlconect/createTable.js
@@ -1,30 +1,30 @@
 let { pool } = require("./mysqlConf.js")
 
-// var sql_createDatabase = 'CREATE DATABASE  msdata'
+// var sql_createDatabase = 'CREATE DATABASE msdata'
 // pool.query(sql_createDatabase, (err, result) => {
 //   if (err) throw err
 //   console.log('Database created ...')
 // })
 
-var sql_users = "CREATE TABLE if not exists users (id INT(30), UserName VARCHAR(255), PassWord VARCHAR(255), NickName VARCHAR(255), Signature VARCHAR(255), HeadIcon VARCHAR(255), Sex INT(1), Age INT(3), RoleId INT(6), CreateTime TIMESTAMP(6))";
+var sql_users = "CREATE TABLE if not exists users (id int auto_increment primary key not null, UserName VARCHAR(255), PassWord VARCHAR(255), NickName VARCHAR(255), Signature VARCHAR(255), HeadIcon VARCHAR(255), Sex INT(1), Age INT(3), RoleId INT(6), CreateTime TIMESTAMP(6))";
 pool.query(sql_users, function (err, result) {
   if (err) throw err;
   console.log("users created");
 });
 
-var sql_musics = "CREATE TABLE if not exists musics (id INT(30), Title VARCHAR(255), Author VARCHAR(255), Type INT(2), Cover VARCHAR(255), MusicUrl VARCHAR(255),Lyric VARCHAR(5000), CreateTime TIMESTAMP(6))";
+var sql_musics = "CREATE TABLE if not exists musics (id int auto_increment primary key not null, Title VARCHAR(255), Author VARCHAR(255), Type INT(2), Cover VARCHAR(255), MusicUrl VARCHAR(255),Lyric VARCHAR(5000), CreateTime TIMESTAMP(6))";
 pool.query(sql_musics, function (err, result) {
   if (err) throw err;
   console.log('musics created')
 })
 
-var sql_role = "CREATE TABLE if not exists roles (id INT(30), RoleName VARCHAR(255), Menu VARCHAR(5000), Intro VARCHAR(255), Status INT(10))";
+var sql_role = "CREATE TABLE if not exists roles (id int auto_increment primary key not null, RoleName VARCHAR(255), Menu VARCHAR(5000), Intro VARCHAR(255), Status INT(10))";
 pool.query(sql_role, function (err, result) {
   if (err) throw err;
   console.log('roles created')
 })
 
-var sql_article = "CREATE TABLE if not exists articles (id INT(30), UserId INT(30), Title VARCHAR(255), Author VARCHAR(255), Content VARCHAR(5000), CreateTime TIMESTAMP(6) )";
+var sql_article = "CREATE TABLE if not exists articles (id int auto_increment primary key not null, UserId INT(30), Title VARCHAR(255), Author VARCHAR(255), Content VARCHAR(5000), CreateTime TIMESTAMP(6) )";
 pool.query(sql_article, function (err, result) {
   if (err) throw err;
   console.log('articles created');
