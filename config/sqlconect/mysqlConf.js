566320db8622b9d2826b70aa6bbbcb4d25686476
unknown <1142920583@qq.com>
2021-03-21 21:52:52 +0800

    1

--- config/sqlconect/mysqlConf.js
+++ config/sqlconect/mysqlConf.js
@@ -5,7 +5,7 @@ let mysql_config = { // mysql采用pool连接池基础配置
     host: '127.0.0.1', // 本地搭建则本机ip,远程服务器则远程服务器ip 
     post: '3306',
     user: 'root', // mysql 账户
-    password: 'root', // mysql 密码
+    password: 'dtt123456', // mysql 密码
     database: 'msdata' // 要操作的数据库// 这里等数据库创建之后放开就可以
 }
 let pool = mysql.createConnection(mysql_config); // 创建连接
